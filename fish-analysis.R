5705eb09333ca3eb0cca2ca338df404d51167975
Christopher Chizinski <cchizinski2@unl.edu>
2018-10-19 11:33:31 -0500

    changed cuttoff to 300

--- fish-analysis.R
+++ fish-analysis.R
@@ -7,8 +7,8 @@ source("R/themes.R")
 fishData <-read_csv("data/Gaeta_etal_CLC_data.csv")
 
 fishData %>% 
-  mutate(length_cat = case_when(length >= 200 ~ "big",
-                                length < 200 ~ "small")) -> fishData
+  mutate(length_cat = case_when(length >= 300 ~ "big",
+                                length < 300 ~ "small")) -> fishData
 
 head(fishData)
 
